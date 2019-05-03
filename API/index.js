5b69fa3aa077bb37c3ccdc6a9c89f4f8f784b248
MeetPing <42493917+MeetPing@users.noreply.github.com>
2019-05-03 15:50:27 +0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -7,11 +7,11 @@ const validKeys = [
     // Logging in on your API key
     function key(x) {
         userKey = x;
-    }
-    
-    // When you need to check
+// When you need to check
     if (validKeys.includes(userKey)) {
     console.log('A Valid key')
     } else {
     console.error('invalid key')
     }
+    }
+    
