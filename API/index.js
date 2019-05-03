d50080997c3df6428d8d3e513e6737a57c8969ef
MeetPing <42493917+MeetPing@users.noreply.github.com>
2019-05-03 15:38:47 +0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -3,7 +3,7 @@ let validKeys = [
     "H72@vhVJu26$RvJ*w4D@rY%WRt!He2X_3e_dsg^n","p&DvwYV9qWq?MAdED@9tQtbEbtxgxUxprVq!p-C#"
     ]; 
     // Logging in on your API key
-    let apikey;
+    let apikeya;
     function key(x) {
       apikey = x;
     }
