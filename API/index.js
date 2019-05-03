627b253e87e6d15b61909e1554389dfd66514b76
MeetPing <42493917+MeetPing@users.noreply.github.com>
2019-05-03 15:34:09 +0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,13 +1,15 @@
+//making validkeys
 let validKeys = [
-"H72@vhVJu26$RvJ*w4D@rY%WRt!He2X_3e_dsg^n","p&DvwYV9qWq?MAdED@9tQtbEbtxgxUxprVq!p-C#"
-];
-// Logging in on your API key
-function key(x) {
-    const apikey = x;
-   }
-// Api usage
-if (apikey.value = validKeys) {
-console.log('A Valid key')
-} else {
-console.log('invaid key')
-}
+    "H72@vhVJu26$RvJ*w4D@rY%WRt!He2X_3e_dsg^n","p&DvwYV9qWq?MAdED@9tQtbEbtxgxUxprVq!p-C#"
+    ]; 
+    // Logging in on your API key
+    let apikey;
+    function key(x) {
+      apikey = x;
+    }
+    // Api usage
+    if (validKeys.includes(apikey.value)) {
+    console.log('A Valid key')
+    } else {
+    console.error('invalid key')
+    }
