cae148d92d5cb9de2d54abd9d7b9b6485185d83f
MeetPing <42493917+MeetPing@users.noreply.github.com>
2019-05-03 15:40:00 +0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,14 +1,16 @@
-//making validkeys
-let validKeys = [
-    "H72@vhVJu26$RvJ*w4D@rY%WRt!He2X_3e_dsg^n","p&DvwYV9qWq?MAdED@9tQtbEbtxgxUxprVq!p-C#"
-    ]; 
+const validKeys = [
+    "key1","key2"
+    ];
+    
+    let userKey;
+    
     // Logging in on your API key
-    let apikeya;
     function key(x) {
-      apikey = x;
+        userKey = x;
     }
-    // Api usage
-    if (validKeys.includes(apikey.value)) {
+    
+    // When you need to check
+    if (validKeys.includes(userKey)) {
     console.log('A Valid key')
     } else {
     console.error('invalid key')
